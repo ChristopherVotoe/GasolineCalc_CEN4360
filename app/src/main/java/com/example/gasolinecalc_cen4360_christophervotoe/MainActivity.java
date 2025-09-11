9206efa8629f5b207d725289d4deb3bdc30487f6
ChristopherV <Chris.Votoe.Official@gmail.com>
2025-09-11 12:47:00 -0400

    Working on logic

--- app/src/main/java/com/example/gasolinecalc_cen4360_christophervotoe/MainActivity.java
+++ app/src/main/java/com/example/gasolinecalc_cen4360_christophervotoe/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.gasolinecalc_cen4360_christophervotoe;
 
 import android.os.Bundle;
+import android.os.DropBoxManager;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -28,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        //int modifier = 0;
 
         distanceInput = findViewById(R.id.distanceNumber);
         costInput     = findViewById(R.id.CPGNumber);
@@ -39,17 +44,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-    private void grabInputedNumbers() {
-        try {
-            int distance = Integer.parseInt(distanceInput.getText().toString().trim());
-            double costPerGallon = Double.parseDouble(costInput.getText().toString().trim());
-            int highwayMPG = Integer.parseInt(mpgInput.getText().toString().trim());
-            Log.d("INPUTS", "distance=" + distance + " cost=" + costPerGallon + " mpg=" + highwayMPG);
-        } catch (NumberFormatException e) {
-            Log.d("Input Error","Numbers entered incorrectly");
-        }
-    }
     private void printValue() {
         String dist = distanceInput.getText().toString().trim();
         String cost = costInput.getText().toString().trim();
@@ -59,5 +53,56 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "distance=" + dist + ", cost=" + cost + ", mpg=" + mpg,
                 Toast.LENGTH_SHORT).show();
     }
+
+    private int calcModifierValue(){
+        int modifier = 0;
+        CheckBox agresiveDriver = findViewById(R.id.AG_Checkbox);
+
+        RadioGroup AC = findViewById(R.id.acRadioGroup);
+        Spinner roadType = findViewById(R.id.RoadType_DropBox);
+        String roadTypeValue = roadType.getSelectedItem().toString();
+        int selectedChoice = AC.getCheckedRadioButtonId();
+
+
+        if(agresiveDriver.isChecked())
+        {
+            switch (roadTypeValue) {
+                case "Highway":
+                    modifier+=15;
+                    break;
+                case "City":
+                    modifier+=25;
+                    break;
+                case "Mixed":
+                    modifier+=20;
+                    break;
+            }
+        }
+        else
+        {
+            //Driver is not aggressive
+            switch (roadTypeValue) {
+                case "Highway":
+                    modifier+=0;
+                    break;
+                case "City":
+                    modifier+=15;
+                    break;
+                case "Mixed":
+                    modifier+=10;
+                    break;
+            }
+        }
+
+        if(selectedChoice == R.id.Yes)
+        {
+            modifier+=15;
+        }
+
+        return modifier;
+    }
+
+
+
 
 }
